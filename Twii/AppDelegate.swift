6bb64b161f6630d75fe3a95cdadde96f918afb4a
Hank <hankhe@gmail.com>
2018-01-24 16:22:49 +0800

    add HomeController

--- Twii/AppDelegate.swift
+++ Twii/AppDelegate.swift
@@ -18,7 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
-        window?.rootViewController = UINavigationController(rootViewController: ViewController())
+        let homeController = HomeController(collectionViewLayout: UICollectionViewFlowLayout())
+        window?.rootViewController = UINavigationController(rootViewController: homeController)
         
         return true
     }
