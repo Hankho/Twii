e05b444fcdce156fee48bff170ad2d7128d3a65e
Hank <hankhe@gmail.com>
2018-01-30 14:59:00 +0800

    User cell subview Layout

--- Twii/Controller/HomeDatasourceController.swift
+++ Twii/Controller/HomeDatasourceController.swift
@@ -11,15 +11,21 @@ import LBTAComponents
 class HomeDatasourceController: DatasourceController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         let homeDatasource = HomeDatasource()
         self.datasource = homeDatasource
         
     }
+    
+    override func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 150)
+    }
+    
     // 定義Header size
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
         return CGSize(width: view.frame.width, height: 50)
     }
-    
+    // 定義Footer size
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
        return CGSize(width: view.frame.width, height: 100)
     }
