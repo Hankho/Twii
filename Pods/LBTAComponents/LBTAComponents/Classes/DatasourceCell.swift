c096ce72b7ccefe4d3ac6f75e5924bfb0ae6fd90
Hank <hankhe@gmail.com>
2018-01-31 17:18:03 +0800

    modify heightConstant

--- Pods/LBTAComponents/LBTAComponents/Classes/DatasourceCell.swift
+++ Pods/LBTAComponents/LBTAComponents/Classes/DatasourceCell.swift
@@ -30,7 +30,7 @@ open class DatasourceCell: UICollectionViewCell {
     open func setupViews() {
         clipsToBounds = true
         addSubview(separatorLineView)
-        separatorLineView.anchor(nil, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0.5)
+        separatorLineView.anchor(nil, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 1)
     }
         
     public required init?(coder aDecoder: NSCoder) {
